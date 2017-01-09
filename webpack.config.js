d886c19b262eb727797540221033afba21667ea8
김영재 <yjinzum@zuminternet.com>
2017-01-09 18:12:00 +0900

    webpack설정

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,23 @@
  */
 const path = require('path');
 const webpack = require('webpack');
+const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpackMerge = require('webpack-merge');
 
-//var __dirname = './src/main/resources/static';
-module.exports = {
+const target = process.env.npm_lifecycle_event;
+const common = {
     entry: path.join(__dirname + '/src/main/resources/static/entry/entry.js'),
     output: {
         path: path.join(__dirname, '/src/main/resources/static'),
         filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.css'],
+        alias: {
+            'styles': __dirname + '/static/styles',
+            'images': __dirname + '/static/images'
+        }
     },
     module: {
         preLoaders: [
@@ -27,12 +37,48 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
+            },
+            {
+                test: /\.(css|scss)$/,
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: "style-loader",
+                    loader: "css-loader!postcss-loader!sass-loader"
+                })
+            },
+            {
+                test: /\.(gif|png|jpg)$/,
+                loader: 'file-loader?name=images/[name].[ext]&mimeType=image/[ext]&limit=100000'
             }
         ]
     },
     plugins: [
-        //new webpack.optimize.OccurenceOrderPlugin(),
-        //new webpack.HotModuleReplacementPlugin(),
-        //new webpack.NoErrorsPlugin()
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new CommonsChunkPlugin({
+            name: "commons",
+            filename: "common.js",
+            minChunks: Infinity,
+        })
     ]
 }
+
+const prodConfig = {
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {warnings: false}
+        }),
+        new ExtractTextPlugin("[name].css")
+    ]
+}
+
+var config;
+if(target === 'watch') {
+    console.log('watch build');
+    config = common;
+} else {
+    console.log('real build');
+    config = webpackMerge(common, prodConfig);
+}
+
+module.exports = config;
