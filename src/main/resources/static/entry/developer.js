84fbd4caac21adbf6405763a335e5da72d3a4617
youngjae <young891221@gmail.com>
2017-01-21 23:51:01 +0900

    class를 파보자!

--- src/main/resources/static/entry/developer.js
+++ src/main/resources/static/entry/developer.js
@@ -1,13 +1,25 @@
 class Developer {
-    constructor(keyboard, monitor) {
-        this.keyboard = keyboard;
-        this.monitor = monitor;
+    constructor(keyboard, monitor, project='none', arrayProject=[]) {
+        this._keyboard = keyboard;
+        this._monitor = monitor;
+        this._project = project;
+        this._arrayProject = arrayProject; //제너레이터 적용하기!
     }
 
-    getDeveloper() {
-        let message = 'My keyboard is ' + this.keyboard + ' and my monitor is ' + this.monitor;
+    getDeveloperItem() {
+        let message = 'My keyboard is ' + this._keyboard + ' and my monitor is ' + this._monitor;
         return message;
     }
+
+    get project() {
+        return this._project;
+    }
+
+    set project(value) {
+        this._project = value;
+    }
+
+
 }
 
 export default Developer;
--- src/main/resources/static/entry/entry.js
+++ src/main/resources/static/entry/entry.js
@@ -9,9 +9,13 @@ let main = {hello: hello};
 main.word = world;
 
 let dev = new developer('hansung', 'dell');
-main.dev = dev.getDeveloper();
+main.dev = dev.getDeveloperItem();
 
 document.write(`${hello}, ${world}!`);
 document.write(main.dev);
+document.write('set 이전의 project = '+dev.project);
+dev.project = 'SpringBoot-Webpack';
+document.write('set 이후의 project = '+dev.project);
+
 
 export default main.dev;
