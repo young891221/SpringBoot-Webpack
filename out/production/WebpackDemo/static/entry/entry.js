806b307eb406fba01d7c9bc9dadd644ef741e827
김영재 <yjinzum@zuminternet.com>
2017-03-24 17:06:31 +0900

    필요없는 폴더 삭제

--- out/production/WebpackDemo/static/entry/entry.js
+++ out/production/WebpackDemo/static/entry/entry.js
@@ -3,5 +3,19 @@
  */
 import hello from  './hello';
 import world from  './world';
+import developer from  './developer';
+
+let main = {hello: hello};
+main.word = world;
+
+let dev = new developer('hansung', 'dell');
+main.dev = dev.getDeveloperItem();
 
 document.write(`${hello}, ${world}!`);
+document.write(main.dev);
+//document.write('set 이전의 project = '+dev.project);
+dev.project = 'SpringBoot-Webpack';
+//document.write('set 이후의 project = '+dev.project);
+
+
+export default main.dev;
